a1fddfdb826b42e8cb124c8e7f2272aeb03a218f
Krishna-anhsirk <Krishnatejajinka@gmail.com>
2022-10-20 15:49:30 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,71 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import logo from "./images/brewery.png";
+
+const BrewCard = ({ item }) => {
+  return (
+    <div className="shop-card">
+      <p>
+        <strong>Name: </strong> {item.name}{" "}
+      </p>
+      <p>
+        <strong>Type: </strong> {item.brewery_type}{" "}
+      </p>
+      <p>
+        <strong>Address: </strong> {item.street},{item.city},{item.state} -{" "}
+        {item.postal_code}{" "}
+      </p>
+      {item.website_url && (
+        <p>
+          <strong>Website: </strong> {item.website_url}
+        </p>
+      )}
+      {item.phone && (
+        <p>
+          <strong>Phn: </strong> {item.phone}
+        </p>
+      )}
+    </div>
+  );
+};
+
+const BrewList = () => {
+  const [brewList, setBrewList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.openbrewerydb.org/breweries")
+      .then((response) => response.json())
+      .then((data) => setBrewList(data));
+  }, []);
+
+  return (
+    <div className="brew-page">
+      {brewList.map((item) => {
+        return <BrewCard item={item} key={item.id} />;
+      })}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <div className="heading">
+      {/* Header */}
+      <header className="heading">
         <img className="logo" src={logo} alt="brew-worry-logo" />
-        <h1>Brew worry</h1>
-      </div>
+        <h1>Brew life</h1>
+      </header>
+      <nav className="filter-nav">
+        <strong>List of Breweries around you</strong>
+      </nav>
+
+      {/* Content */}
+      <BrewList />
+
+      {/* Footer  */}
       <footer>
-        <p>© 2022</p>
+        <p>© Oct 2022</p>
+        <p>Made with ❤️ using react</p>
       </footer>
     </div>
   );
